48c16e9e0335fddab61808ed882aa7c69cd6cd70
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-03 10:52:25 -0400

    Added skeleton code for routing and controllers

---
+++ server/controllers/posts.js
@@ -0,0 +1,13 @@
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation');
+}
