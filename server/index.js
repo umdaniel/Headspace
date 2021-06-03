2d1590ef4887455951ef879d91a54a73982b6c78
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-03 10:52:48 -0400

    Added skeleton code for routing and controllers

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 import express from 'express';
-import bodyParser from 'bodyparser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-// Connecting to MongoDB database.
+const CONNECTION_URL = 'mongodb+srv://umdaniel12:Orange1211@headspace.o0at2.mongodb.net/Headspace?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
