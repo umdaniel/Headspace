6cb32a8043c1bdc85272c8d24c22999deb87d89d
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-06 12:58:14 -0400

    Fixed an error related to proxy

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://umdaniel12:Orange1211@headspace.o0at2.mongodb.net/Headspace?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
