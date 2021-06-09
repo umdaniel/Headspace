6efe39910c3f78fe445fa5d8634cd7350b200407
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-08 20:43:26 -0400

    circular progress testing

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Post from './Post/Post';
+import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Posts = () => {
+    const posts = useSelector((state) => state.posts);
+    const classes = useStyles();
+    
+    console.log(posts);
+    return(
+        !posts.length ? <CircularProgress /> : (
+            <Grid>
+                <CircularProgress />
+            </Grid>
+        )
+    );
+}
+
+export default Posts;
