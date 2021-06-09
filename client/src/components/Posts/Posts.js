d8c67d046d3b86a6f22f490278d9a38b5c933e1f
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-08 20:44:37 -0400

    circular progress testing

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -13,7 +13,7 @@ const Posts = () => {
     return(
         !posts.length ? <CircularProgress /> : (
             <Grid>
-
+                <CircularProgress />
             </Grid>
         )
     );
