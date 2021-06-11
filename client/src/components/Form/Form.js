38e1d179a35b9e270537af0fe79292320363eada
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-10 20:20:11 -0400

    Memory creation functionality

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
 import useStyles from './styles';
 
-const Form = () => {
+// Get the current ID.
+
+const Form = ({currentId, setCurrentId}) => {
     const [postData, setPostData] = useState({
         creator: '', title: '', message: '', tags: '', selectedFile: '' });
     const classes = useStyles();
@@ -14,6 +16,12 @@ const Form = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (currentId) {
+            dispatch(updatePost(currentId, postData));
+        } else {
+            dispatch(createPost(postData));
+        }
 
         dispatch(createPost(postData));
     }
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import './index.css';
+
 import App from './App';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
